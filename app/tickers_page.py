9e41e82d9ce0992265d5c10a75789bb793a9b9dd
Gustavo Suto <guyrux@gmail.com>
2021-03-27 18:05:24 -0300

    :construction: Created a new app page.
    - It is just a blank page.

---
+++ app/tickers_page.py
@@ -0,0 +1,13 @@
+# import configparser
+# from datetime import date, timedelta
+
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import seaborn as sns
+import streamlit as st
+
+# from src.data_acquisition.collect_tickers_data import TickersCollector, DataCollector
+
+
+def tickers_indicator():
+    st.markdown("# Nothing to show.\n\nYet...")
